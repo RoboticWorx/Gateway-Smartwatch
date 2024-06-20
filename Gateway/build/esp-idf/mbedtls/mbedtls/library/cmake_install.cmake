aac8d14607b11fbcb13d32762cb320f2236fae47
RoboticWorx <roboticworx@gmail.com>
2024-06-20 13:51:29 -0400

    Gateway Code

---
+++ Gateway/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake
@@ -0,0 +1,51 @@
+# Install script for directory: C:/Espressif/frameworks/esp-idf-v5.1.2/components/mbedtls/mbedtls/library
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/main")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/Espressif/tools/xtensa-esp32s3-elf/esp-12.2.0_20230208/xtensa-esp32s3-elf/bin/xtensa-esp32s3-elf-objdump.exe")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/Jatki/OneDrive/Documents/ESP/Gateway/build/esp-idf/mbedtls/mbedtls/library/libmbedcrypto.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/Jatki/OneDrive/Documents/ESP/Gateway/build/esp-idf/mbedtls/mbedtls/library/libmbedx509.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/Jatki/OneDrive/Documents/ESP/Gateway/build/esp-idf/mbedtls/mbedtls/library/libmbedtls.a")
+endif()
+
---
+++ Gateway/components/st7789/st7789.h
@@ -0,0 +1,89 @@
+#ifndef MAIN_ST7789_H_
+#define MAIN_ST7789_H_
+
+#include "driver/spi_master.h"
+#include "fontx.h"
+
+#define rgb565(r, g, b) (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3))
+
+#define RED    rgb565(255,   0,   0) // 0xf800
+#define GREEN  rgb565(  0, 255,   0) // 0x07e0
+#define BLUE   rgb565(0, 71, 171) // 0x001f #define COBALT
+#define BLACK  rgb565(  0,   0,   0) // 0x0000
+#define WHITE  rgb565(255, 255, 255) // 0xffff
+#define GRAY   rgb565(128, 128, 128) // 0x8410
+#define YELLOW rgb565(255, 255,   0) // 0xFFE0
+#define CYAN   rgb565(  0, 156, 209) // 0x04FA
+#define PURPLE rgb565(128,   0, 128) // 0x8010
+
+typedef enum {DIRECTION0, DIRECTION90, DIRECTION180, DIRECTION270} DIRECTION;
+
+typedef enum {
+	SCROLL_RIGHT = 1,
+	SCROLL_LEFT = 2,
+	SCROLL_DOWN = 3,
+	SCROLL_UP = 4,
+} SCROLL_TYPE_t;
+
+typedef struct {
+	uint16_t _width;
+	uint16_t _height;
+	uint16_t _offsetx;
+	uint16_t _offsety;
+	uint16_t _font_direction;
+	uint16_t _font_fill;
+	uint16_t _font_fill_color;
+	uint16_t _font_underline;
+	uint16_t _font_underline_color;
+	int16_t _dc;
+	int16_t _bl;
+	spi_device_handle_t _SPIHandle;
+	bool _use_frame_buffer;
+	uint16_t *_frame_buffer;
+} TFT_t;
+
+void spi_master_init(TFT_t * dev, int16_t GPIO_MOSI, int16_t GPIO_SCLK, int16_t GPIO_CS, int16_t GPIO_DC, int16_t GPIO_RESET, int16_t GPIO_BL);
+bool spi_master_write_byte(spi_device_handle_t SPIHandle, const uint8_t* Data, size_t DataLength);
+bool spi_master_write_command(TFT_t * dev, uint8_t cmd);
+bool spi_master_write_data_byte(TFT_t * dev, uint8_t data);
+bool spi_master_write_data_word(TFT_t * dev, uint16_t data);
+bool spi_master_write_addr(TFT_t * dev, uint16_t addr1, uint16_t addr2);
+bool spi_master_write_color(TFT_t * dev, uint16_t color, uint16_t size);
+bool spi_master_write_colors(TFT_t * dev, uint16_t * colors, uint16_t size);
+
+void delayMS(int ms);
+void lcdInit(TFT_t * dev, int width, int height, int offsetx, int offsety);
+void lcdDrawPixel(TFT_t * dev, uint16_t x, uint16_t y, uint16_t color);
+void lcdDrawMultiPixels(TFT_t * dev, uint16_t x, uint16_t y, uint16_t size, uint16_t * colors);
+void lcdDrawFillRect(TFT_t * dev, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void lcdDisplayOff(TFT_t * dev);
+void lcdDisplayOn(TFT_t * dev);
+void lcdFillScreen(TFT_t * dev, uint16_t color);
+void lcdDrawLine(TFT_t * dev, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void lcdDrawRect(TFT_t * dev, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void lcdDrawRectAngle(TFT_t * dev, uint16_t xc, uint16_t yc, uint16_t w, uint16_t h, uint16_t angle, uint16_t color);
+void lcdDrawTriangle(TFT_t * dev, uint16_t xc, uint16_t yc, uint16_t w, uint16_t h, uint16_t angle, uint16_t color);
+void lcdDrawRegularPolygon(TFT_t *dev, uint16_t xc, uint16_t yc, uint16_t n, uint16_t r, uint16_t angle, uint16_t color);
+void lcdDrawCircle(TFT_t * dev, uint16_t x0, uint16_t y0, uint16_t r, uint16_t color);
+void lcdDrawFillCircle(TFT_t * dev, uint16_t x0, uint16_t y0, uint16_t r, uint16_t color);
+void lcdDrawRoundRect(TFT_t * dev, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t r, uint16_t color);
+void lcdDrawArrow(TFT_t * dev, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t w, uint16_t color);
+void lcdDrawFillArrow(TFT_t * dev, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t w, uint16_t color);
+int lcdDrawChar(TFT_t * dev, FontxFile *fx, uint16_t x, uint16_t y, uint8_t ascii, uint16_t color);
+int lcdDrawString(TFT_t * dev, FontxFile *fx, uint16_t x, uint16_t y, uint8_t * ascii, uint16_t color);
+int lcdDrawCode(TFT_t * dev, FontxFile *fx, uint16_t x,uint16_t y,uint8_t code,uint16_t color);
+//int lcdDrawUTF8Char(TFT_t * dev, FontxFile *fx, uint16_t x, uint16_t y, uint8_t *utf8, uint16_t color);
+//int lcdDrawUTF8String(TFT_t * dev, FontxFile *fx, uint16_t x, uint16_t y, unsigned char *utfs, uint16_t color);
+void lcdSetFontDirection(TFT_t * dev, uint16_t);
+void lcdSetFontFill(TFT_t * dev, uint16_t color);
+void lcdUnsetFontFill(TFT_t * dev);
+void lcdSetFontUnderLine(TFT_t * dev, uint16_t color);
+void lcdUnsetFontUnderLine(TFT_t * dev);
+void lcdBacklightOff(TFT_t * dev);
+void lcdBacklightOn(TFT_t * dev);
+void lcdInversionOff(TFT_t * dev);
+void lcdInversionOn(TFT_t * dev);
+void lcdWrapArround(TFT_t * dev, SCROLL_TYPE_t scroll, int start, int end);
+void lcdDrawFinish(TFT_t *dev);
+#endif /* MAIN_ST7789_H_ */
+
